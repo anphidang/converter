0b2129c7dbf7426afd2056256998f701a6e6560f
eninjaergy <96066458+eninjaergy@users.noreply.github.com>
2025-03-17 20:59:42 +0100

    hochladen der Basic Datei

---
+++ currency.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+  int pesos;
+  int reais;
+  int soles;
+  int dollars;
+  std::cout << "Enter number of Colombian Pesos:
+";
+  std::cin >> pesos;
+  std::cout << "Enter number of Brazilian Reais:
+";
+  std::cin >> reais;
+  std::cout << "Enter number of Peruvian Soles:
+";
+  std::cin >> soles;
+
+  //peso conversion rate 0.049
+  //reais conversion rate 0.17
+  //soles conversion rate 0.26
+
+dollars = (0.049*pesos + 0.17*reais + 0.26*soles);
+std::cout << "US Dollars = $ " << dollars;
+}
